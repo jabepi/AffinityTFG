b9bcfdf1d17f434e46b405fda3a6ba6e7affb8f9
jabepi <javier.beiro@hotmail.com>
2023-04-18 17:19:45 +0200

    generalización

--- ejec.sh
+++ ejec.sh
@@ -1,21 +1,25 @@
 
-archivo="affinity"
+clear
+archivo="affinity0"
 g++ "$archivo.cpp" -lnuma -fopenmp -O0 -o "$archivo.out"
 
-#Tamaños de L1, L2, L3
-#tam="32768 262144 26214400"
-tam="8192 16384 32768 65526 131072 262144 524288 1048576 26214400 52428800 104857600 104857600 209715200"
+#./"$archivo.out" args salida
 
-echo "-------------------Hilos muy afines------------------- " >> salidaVisual.txt
-for i in $tam
-    do 
-    #perf record -e LLC-load-misses,LLC-loads,LLC-store-misses,LLC-stores,L1-dcache-load-misses,L1-dcache-loads,L1-dcache-stores,l2_rqsts.all_demand_miss,l2_rqsts.all_demand_references -o "datosA$i.data" 
-    ./"$archivo.out" 2 0 0 0 $i salida
-done
+# #Tamaños de L1, L2, L3
+# #tam="32768 262144 26214400"                                                                  
+# #tam="8192 16384 32768 65526 131072 262144 524288 1048576 26214400 52428800 104857600 209715200"
+# tam="209715200"
 
-echo "-------------------Hilos muy poco afines-------------------" >> salidaVisual.txt
-for i in $tam
-    do 
-    #perf record -e LLC-load-misses,LLC-loads,LLC-store-misses,LLC-stores,L1-dcache-load-misses,L1-dcache-loads,L1-dcache-stores,l2_rqsts.all_demand_miss,l2_rqsts.all_demand_references -o "datosNA$i.data"  
-    ./"$archivo.out" 2 0 2 0 $i salida
-done
+# # echo "-------------------Hilos muy afines------------------- " >> salidaVisual.txt
+# # for i in $tam
+# #     do 
+# #     #perf record -e LLC-load-misses,LLC-loads,LLC-store-misses,LLC-stores,L1-dcache-load-misses,L1-dcache-loads,L1-dcache-stores,l2_rqsts.all_demand_miss,l2_rqsts.all_demand_references -o "datosA$i.data" 
+# #     ./"$archivo.out" 2 0 0 2 $i salidaAfines2Visual
+# # done
+
+# #echo "-------------------Hilos muy poco afines-------------------" >> salidaVisual.txt
+# for i in $tam
+#     do 
+#     #perf record -e LLC-load-misses,LLC-loads,LLC-store-misses,LLC-stores,L1-dcache-load-misses,L1-dcache-loads,L1-dcache-stores,l2_rqsts.all_demand_miss,l2_rqsts.all_demand_references -o "datosNA$i.data"  
+#     ./"$archivo.out" 4 2 2 0 0 0 $i salida5
+# done
--- prueba.cpp
+++ prueba.cpp
@@ -21,3 +21,109 @@ int main()
     for (auto [x, y] : hist)
         std::cout << std::setw(2) << x << ' ' << std::string(y / 200, '*') << '\n';
 }
+
+
+	vector<size_t> list_thr_node;
+	size_t thread_num;
+	long psets_size;
+	long ssets_size;
+	short sset_node;
+
+	//-------------------------------------------Temporal------------------------------------------------
+	//TODO son muchas combinaciones y datos, mejor leer de un archivo y parsearlo, para que sea más legible 
+	if(argc < 2){
+		cout << "Introducir nodo de vector compartido." << endl;
+		return 0;
+	}
+	//Número de hilos
+	thread_num = atoi(argv[1]);
+	int i = 0;
+	//Nodo de cada hilo 
+	while(i < thread_num){
+		list_thr_node.push_back(atoi(argv[i+2]));
+		i++;	
+	}
+	//Nodo del vector compartido
+	sset_node = atoi(argv[i+2]);
+	i++;
+	//Tamaño de los sets privado y compartico
+	ssets_size = atol(argv[i+2]);
+	psets_size = ssets_size;
+
+	//Archivo de salida 
+	string salida = argv[i+3];
+	ofstream outfile(salida+"Visual.txt", std::ios::app);
+    if (!outfile.is_open()) {
+       cout << "No se ha podido abrir el archivo " << salida+"Visual.txt" << endl;
+       return 1;
+    }
+	//---------------------------------------------------------------------------------------------------
+
+
+	// //2. Memory reads
+		// i=0;
+		// #pragma omp barrier
+		// start = std::chrono::high_resolution_clock::now();
+		// while(i < ITER){
+		// 	r1 = array.read_private(ssets_size);
+		// 	i++;
+		// }
+		// end = std::chrono::high_resolution_clock::now();
+        // diff = end - start;
+		// r_tmarkp[thread] = diff.count()/ITER;
+		// #pragma omp single
+		// {
+		// 	cout << "Terminadas las lecturas privadas" << endl;
+		// }
+		
+		// i=0;
+		// #pragma omp barrier
+		// start = std::chrono::high_resolution_clock::now();
+		// while(i < ITER){
+		// 	r2 = array.read_shared(ssets_size);
+		// 	i++;
+		// }
+		// end = std::chrono::high_resolution_clock::now();
+        // diff = end - start;
+		// r_tmarks[thread] = diff.count()/ITER;
+		// #pragma omp single
+		// {
+		// 	cout << "Terminadas las lecturas compartidas" << endl;
+		// }
+		
+		// flushCache(list_thr_node[thread]);
+		// doNotOptimizeAway(r1);
+		// doNotOptimizeAway(r2);
+
+		// //3. Memory reads and writes
+		// i=0;
+		// #pragma omp barrier
+		// start = std::chrono::high_resolution_clock::now();
+		// while(i < ITER){
+		// 	rw1 = array.rw_private(ssets_size);
+		// 	i++;
+		// }
+		// end = std::chrono::high_resolution_clock::now();
+		// diff = end - start;
+		// rw_tmarkp[thread] = diff.count()/ITER;
+		// #pragma omp single
+		// {
+		// 	cout << "Terminadas las lecturas y escrituras privadas" << endl;
+		// }
+
+		// i=0;
+		// #pragma omp barrier
+		// start = std::chrono::high_resolution_clock::now();
+		// while(i < ITER){
+		// 	rw2 = array.rw_shared(ssets_size);
+		// 	i++;
+		// }
+		// end = std::chrono::high_resolution_clock::now();
+		// diff = end - start;
+		// rw_tmarks[thread] = diff.count()/ITER;
+		// doNotOptimizeAway(rw1);
+		// doNotOptimizeAway(rw2);
+		// #pragma omp single
+		// {
+		// 	cout << "Terminadas las lecturas y escrituras compartidas" << endl;
+		// }
